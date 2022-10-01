f0bfb5e28e56261cf9882456c1132a27871f70eb
onier9702 <onier0217@gmail.com>
2022-10-01 09:39:03 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,67 @@
+
+// References
+const menuIcon = document.querySelector('#menu');
+const menuIconUl = document.querySelector('#menuUl');
+const unList = document.querySelector('#navig');
+// Sesion references
+const misionSesion = document.querySelector('#misionAncor');
+const visionSesion = document.querySelector('#visionAncor');
+const integrantesSesion = document.querySelector('#integrantesAncor');
+const ppiosSesion = document.querySelector('#ppiosAncor');
+
+// Test smooth behavior
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// variables
+let condition = false;
+
+
+menuIcon.addEventListener( 'click', () =>  {
+    condition = !condition;
+    openCloseMenu();
+} )
+
+menuIconUl.addEventListener( 'click', () =>  {
+    condition = false;
+    openCloseMenu();
+} )
+
+misionSesion.addEventListener( 'click', () => {
+    condition = false;
+    closeMenuAfterClickNavigation();
+} )
+
+visionSesion.addEventListener( 'click', () => {
+    condition = false;
+    closeMenuAfterClickNavigation();
+} )
+
+integrantesSesion.addEventListener( 'click', () => {
+    condition = false;
+    closeMenuAfterClickNavigation();
+} )
+
+ppiosSesion.addEventListener( 'click', () => {
+    condition = false;
+    closeMenuAfterClickNavigation();
+} )
+
+const openCloseMenu = () => {
+    if ( condition ) {
+        unList.classList.add('activate');
+    } else {
+        unList.classList.remove('activate');
+    }
+}
+
+const closeMenuAfterClickNavigation = () => {
+    unList.classList.remove('activate');
+}
