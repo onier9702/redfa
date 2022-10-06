fb6b8eb14d3cea2d0711eda8524ee3d81ec442eb
onier9702 <onier0217@gmail.com>
2022-10-05 17:34:45 -0700

    Other Pages Added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const misionSesion = document.querySelector('#misionAncor');
 const visionSesion = document.querySelector('#visionAncor');
 const integrantesSesion = document.querySelector('#integrantesAncor');
 const ppiosSesion = document.querySelector('#ppiosAncor');
+const actionsSesion = document.querySelector('#actionsAncor');
+const datesSesion = document.querySelector('#datesAncor');
 
 // Test smooth behavior
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -50,6 +52,16 @@ integrantesSesion.addEventListener( 'click', () => {
 } )
 
 ppiosSesion.addEventListener( 'click', () => {
+    condition = false;
+    closeMenuAfterClickNavigation();
+} )
+
+actionsSesion.addEventListener( 'click', () => {
+    condition = false;
+    closeMenuAfterClickNavigation();
+} )
+
+datesSesion.addEventListener( 'click', () => {
     condition = false;
     closeMenuAfterClickNavigation();
 } )
